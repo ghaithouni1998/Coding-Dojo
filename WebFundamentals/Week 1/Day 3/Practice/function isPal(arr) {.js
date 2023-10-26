a56b16b5e1d1550f99c87939a531a3cb79af7d53
ghaithouni1998 <ghaithouni1998>
2023-10-26 14:21:41 +0100

    commit

---
+++ WebFundamentals/Week 1/Day 3/Practice/function isPal(arr) {.js
@@ -0,0 +1,16 @@
+function isPal(arr) {
+    for(var left=0; left<arr.length/2; left++) {
+        var right = arr.length-1-left;
+        if(arr[left] != arr[right]) {
+            return "Not a pal-indrome!";
+        }
+    }
+    return "Pal-indrome!";
+}
+    
+var result1 = isPal( [1, 1, 2, 2, 1] );
+
+
+for (var left=0;left<2.5;left++)
+var right = 4
+if arr[2.5] != 4 
