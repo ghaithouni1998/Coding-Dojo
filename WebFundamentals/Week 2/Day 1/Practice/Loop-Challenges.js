e10f5ac7748c5700937d72b2aa1754161c44163f
ghaithouni1998 <ghaithouni1998>
2023-10-31 15:19:13 +0100

    commit

---
+++ WebFundamentals/Week 2/Day 1/Practice/Loop-Challenges.js
@@ -0,0 +1,15 @@
+for (var i=1 ;i <=20 ; i++)
+{
+    if (i%2==1) console.log(i);
+}
+
+for (var i=100 ; i>=0 ;i--)
+{
+    if (i%3==0) console.log(i);
+} 
+
+for(var i=4 ; i>=-3.5; i-=1.5)console.log(i); 
+
+var sigma=0 
+for  (var i=1 ; i<=100;i++) sigma +=i 
+console.log(sigma)
