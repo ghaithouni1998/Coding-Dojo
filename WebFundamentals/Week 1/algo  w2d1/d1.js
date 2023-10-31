71b858b5304ebd0da1a7085f33759e8bbf5fac35
ghaithouni1998 <ghaithouni1998>
2023-10-31 15:14:33 +0100

    commit

---
+++ WebFundamentals/Week 1/algo  w2d1/d1.js
@@ -0,0 +1,37 @@
+var floor = Math.floor (1.8); 1
+var ceiling = math.ceil(math.PI) 4
+var random= math.random();
+
+
+function d6(){
+    var roll = math.random();
+
+    return roll ;
+}
+var playerroll= d6( 
+    console.log 
+)
+
+3 
+var lifesAnswers = [
+    "It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Yes – definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good.",
+    "Yes.",
+    "Signs point to yes.",
+    "Reply hazy, try again.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    "Don't count on it.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Very doubtful."
+];
