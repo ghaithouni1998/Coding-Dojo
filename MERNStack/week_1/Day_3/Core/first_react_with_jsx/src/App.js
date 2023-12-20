d926e7bdbcb5cde99ced410dae7084ce6a8df923
ghaithouni1998 <ghaithouni1998>
2023-12-20 18:29:26 +0100

    commit

---
+++ MERNStack/week_1/Day_3/Core/first_react_with_jsx/src/App.js
@@ -0,0 +1,22 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+        <fieldset><legend>Hello Dojo !!</legend>
+        <h1>Things i need to do</h1>
+        
+      
+          <ul>
+            <li>Learn React</li>
+            <li>climb mt everest</li>
+            <li>run a marathon</li>
+            <li>feed the dogs</li>
+          </ul>
+        
+      
+        </fieldset>
+  )
+}
+
+export default App;
