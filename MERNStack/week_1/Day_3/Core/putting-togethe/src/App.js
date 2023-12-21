d0b472d4a81a6baf1ab95414f1c68bce16231990
ghaithouni1998 <ghaithouni1998>
2023-12-21 13:33:35 +0100

    commit

---
+++ MERNStack/week_1/Day_3/Core/putting-togethe/src/App.js
@@ -0,0 +1,39 @@
+
+
+import "./App.css";
+import PersonCard from "./components/personcard";
+
+const peopleArr = [
+  {
+    firstName: "ghaith",
+    lastName: "Jane",
+    age: 45,
+    hairColor: "Black",
+  },
+  {
+    firstName: "nawfel",
+    lastName: "John",
+    age: 88,
+    hairColor: "Brown",
+  },
+
+];
+
+function App() {
+  return (
+    <div className="App">
+      {peopleArr.map((personObj, index) => (
+        <PersonCard
+          key={index}
+          firstName={personObj.firstName}
+          lastName={personObj.lastName}
+          age={personObj.age}
+          hairColor={personObj.hairColor}
+        />
+      ))}
+    
+    </div>
+  );
+}
+
+export default App;
