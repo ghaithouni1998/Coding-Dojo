d00f01ff57dcfc4b7460c93bb447e8a7f289c235
ghaithouni1998 <ghaithouni1998>
2023-12-22 11:33:04 +0100

    commit

--- MERNStack/week_1/Day_4/Core/more-froms/src/componets/form.jsx
+++ MERNStack/week_1/Day_4/Core/more-froms/src/componets/form.jsx
@@ -2,12 +2,12 @@ import React, { useState } from 'react'
 
 const Form = () => {
 
-    const [ firstname, setFirstname ] = useState('')
-    const [ lastname, Setlastname ] = useState('')
-    const [ email, Setemail ] = useState('')
-    const [ password, Setpassword ] = useState('')
-    const [ confirmpassword, Setconfirmpassword ] = useState('')
-    
+    const [firstname, setFirstname] = useState('')
+    const [lastname, Setlastname] = useState('')
+    const [email, Setemail] = useState('')
+    const [password, Setpassword] = useState('')
+    const [confirmpassword, Setconfirmpassword] = useState('')
+
     const createUser = (e) => {
         // we must prevent the default refresh of the browser to keep our state from being reset
         e.preventDefault();
@@ -15,35 +15,35 @@ const Form = () => {
 
     return (
         <fieldset>
-        <form  onSubmit={ createUser }>
-        <div>
-                <label>firstname :</label> 
-                <input type="text" onChange={ (e) => setFirstname(e.target.value) } />
-                {firstname.length<2?<p >firstname must be at least 2</p>:<p > firstname Valid!</p>}
-            </div>
-            <div>
-                <label>lastname : </label> 
-                <input type="text" value={lastname} onChange={ (e) => Setlastname(e.target.value) } />
-                {lastname.length<2?<p >lastname must be at least 2</p>:<p > lastname Valid!</p>}
-            </div>
-            <div>
-                <label> email : </label> 
-                <input type="email" value={email} onChange={ (e) => Setemail(e.target.value) } />
-                {email.length<2?<p >email must be at zleast 2</p>:<p > email Valid!</p>}
-            </div>
-            <div>
-                <label> password :</label> 
-                <input type="password" value={password} onChange={ (e) => Setpassword(e.target.value) } />
-                {password.length<8?<p >password must be at least 8 </p>:<p > password Valid!</p>}
-            </div>
-            <div>
-                <label> confirmpassword : </label> 
-                <input type="password" value={confirmpassword} onChange={ (e) => Setconfirmpassword(e.target.value) } />
-                {password.length<8?<p >password must match </p>:<p > password Valid!</p>}
-            </div>
-        </form>
-       
-       
+            <form onSubmit={createUser}>
+                <div>
+                    <label>firstname :</label>
+                    <input type="text" onChange={(e) => setFirstname(e.target.value)} />
+                    {firstname.length < 2 ? <p >firstname must be at least 2</p> : <p > firstname Valid!</p>}
+                </div>
+                <div>
+                    <label>lastname : </label>
+                    <input type="text" value={lastname} onChange={(e) => Setlastname(e.target.value)} />
+                    {lastname.length < 2 ? <p >lastname must be at least 2</p> : <p > lastname Valid!</p>}
+                </div>
+                <div>
+                    <label> email : </label>
+                    <input type="email" value={email} onChange={(e) => Setemail(e.target.value)} />
+                    {email.length < 2 ? <p >email must be at zleast 2</p> : <p > email Valid!</p>}
+                </div>
+                <div>
+                    <label> password :</label>
+                    <input type="password" value={password} onChange={(e) => Setpassword(e.target.value)} />
+                    {password.length < 8 ? <p >password must be at least 8 </p> : <p > password Valid!</p>}
+                </div>
+                <div>
+                    <label> confirmpassword : </label>
+                    <input type="password" value={confirmpassword} onChange={(e) => Setconfirmpassword(e.target.value)} />
+                    <p style={{color:"red"}}>{confirmpassword!=password?"Passwords mast match !":""}</p>
+                    {/* <input type="password" onChange={(e) => setUserCPass(e.target.value)} /> */}
+                </div>
+            </form>
+
         </fieldset>
     )
 }
