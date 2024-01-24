822c1e3b133e445f3a44e47ce3de6bc7035ffa77
ghaithouni1998 <ghaithouni1998>
2024-01-24 15:15:54 +0100

    commit

---
+++ Java/Java/Week1/Day1/Practice/first java/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main (String[]args){
+        System.out.println("My name is ghaith  ouni ");
+        System.out.println("i am 25 years old ");
+        System.out.println("My hometown is Burbank ");
+        
+
+    }
+}
