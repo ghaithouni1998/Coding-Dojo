6e08cff9bcef27421114cec097b90e0815ea5063
ghaithouni1998 <ghaithouni1998>
2024-01-28 20:06:39 +0100

    commit

---
+++ Java/Java/Week1/Day2/Practice/Barista's Challenge/Item.java
@@ -0,0 +1,24 @@
+class Item {
+    private String name;
+    private double price;
+    // ALL ARGS CONSTRUCTOR
+    public Item(String name,double price){
+        this.name=name;
+        this.price=price;
+    } 
+
+    // GETTERS & SETTERS
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String newValue){
+        this.name=newValue;
+    }
+    public double getPrice(){
+        return this.price;
+    }
+    public void setPrice(double newPrice){
+        this.price=newPrice;
+    }
+
+}
