1fb91fc1dba76d92f8b39fcaa962a3734500cbde
ghaithouni1998 <ghaithouni1998>
2024-05-26 17:12:47 +0100

    commit

--- Java/Java/Week2/Day2/Practice/Books-API/src/main/java/com/example/book/controllers/BookController.java
+++ Java/Java/Week2/Day2/Practice/Books-API/src/main/java/com/example/book/controllers/BookController.java
@@ -1,5 +1,33 @@
 package com.example.book.controllers;
 
-public class BookController {
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.book.models.Book;
+import com.example.book.services.BookService;
+@RestController
+public class BookController {
+ private final BookService bookService;
+ public BookController(BookService bookService){
+     this.bookService = bookService;
+ }
+ // other methods removed for brevity
+ @RequestMapping(value="/api/books/{id}", method=RequestMethod.PUT)
+ public Book update(
+ 		@PathVariable("id") Long id, 
+ 		@RequestParam(value="title") String title, 
+ 		@RequestParam(value="description") String desc, 
+ 		@RequestParam(value="language") String lang,
+ 		@RequestParam(value="pages") Integer numOfPages) {
+     Book book = bookService.updateBook(id, title, desc, lang, numOfPages);
+     return book;
+ }
+ 
+ @RequestMapping(value="/api/books/{id}", method=RequestMethod.DELETE)
+ public void destroy(@PathVariable("id") Long id) {
+     bookService.deleteBook(id);
+ }
 }
--- Java/Java/Week2/Day2/Practice/Books-API/src/main/java/com/example/book/services/BookService.java
+++ Java/Java/Week2/Day2/Practice/Books-API/src/main/java/com/example/book/services/BookService.java
@@ -10,27 +10,46 @@ import com.example.book.repositories.BookRepository;
 
 @Service
 public class BookService {
-    // adding the book repository as a dependency
-    private final BookRepository bookRepository;
-    
-    public BookService(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
-    }
-    // returns all the books
-    public List<Book> allBooks() {
-        return bookRepository.findAll();
-    }
-    // creates a book
-    public Book createBook(Book b) {
-        return bookRepository.save(b);
-    }
-    // retrieves a book
-    public Book findBook(Long id) {
-        Optional<Book> optionalBook = bookRepository.findById(id);
-        if(optionalBook.isPresent()) {
-            return optionalBook.get();
-        } else {
-            return null;
-        }
-    }
+	// adding the book repository as a dependency
+	private final BookRepository bookRepository;
+
+	public BookService(BookRepository bookRepository) {
+		this.bookRepository = bookRepository;
+	}
+
+	// returns all the books
+	public List<Book> allBooks() {
+		return bookRepository.findAll();
+	}
+
+	// creates a book
+	public Book createBook(Book b) {
+		return bookRepository.save(b);
+	}
+
+	// retrieves a book
+	public Book findBook(Long id) {
+		Optional<Book> optionalBook = bookRepository.findById(id);
+		if (optionalBook.isPresent()) {
+			return optionalBook.get();
+		} else {
+			return null;
+		}
+	}
+
+	// update a book
+	public Book updateBook(Long id, String title, String desc, String lang, int numOfPages) {
+		Book updatebook = new Book(title, desc, lang, numOfPages);
+		Optional<Book> optionalBook = bookRepository.findById(id);
+		if (optionalBook.isPresent()) {
+			return bookRepository.save(updatebook);
+		} else {
+			return null;
+		}
+	}
+
+	// DELETE
+	public void deleteBook(Long id) {
+		bookRepository.deleteById(id);
+	}
 }
